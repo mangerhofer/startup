0a0affe4e244302703e4ea06c1bd166d86e298b4
McCall Angerhofer <mccallangerhofer@gmail.com>
2023-03-10 22:25:48 -0700

    uploading more js code

--- search.js
+++ search.js
@@ -4,13 +4,16 @@ function search() {
     let input = document.getElementById('searchbar').value
     input=input.toLowerCase();
     let x = document.getElementsByClassName('card-title');
+    let y = document.getElementsByClassName('card-body');
       
     for (i = 0; i < x.length; i++) { 
         if (!x[i].innerHTML.toLowerCase().includes(input)) {
             x[i].style.display="none";
+            y[i].style.display = 'none';
         }
         else {
-            x[i].style.display="list-item";                 
+            x[i].style.display="card"; 
+            y[i].style.display = 'card-body';                
         }
     }
 }
